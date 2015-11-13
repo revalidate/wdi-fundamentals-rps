918c7def07c29ed60c4316680e473c5eab967ae8
Reva <reva@Revas-MacBook-Pro.local>
2015-11-13 00:41:44 -0500

    added expressions to getPlayerMove(x) and getComputerMove(y)

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,19 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return
+    var x = move
+    x = x || 'getInput()'
+    ;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return
+    var y = move
+    y = y || 'randomPlay()';
 }
 
 function getWinner(playerMove,computerMove) {
@@ -52,4 +57,3 @@ function playToFive() {
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
 }
-
