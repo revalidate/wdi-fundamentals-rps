46c128d2ddb8593e5dc3a120e84d20c08a344049
revalidate <reva.bhatt@gmail.com>
2015-11-16 00:04:45 -0800

    added playToFive() code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -17,13 +17,9 @@ function randomPlay() {
         return "scissors";
     }
 }
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    return move || getInput()
-    ;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
@@ -33,15 +29,15 @@ function getComputerMove(move) {
 function getWinner(playerMove,computerMove) {
     var winner;
     if (playerMove === computerMove) {
-        winner = 'tie'
+        winner = 'tie';
     } else if (playerMove === 'rock' && computerMove === 'scissors') {
-        winner = 'player'
+        winner = 'player';
     } else if (playerMove === 'paper' && computerMove === 'rock') {
-        winner = 'player'
+        winner = 'player';
     } else if (playerMove === 'scissors' && computerMove === 'paper') {
-        winner = 'player'
+        winner = 'player';
     } else {
-        winner = 'computer'
+        winner = 'computer';
     }
     return winner;
 }
@@ -50,7 +46,21 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while ((playerWins < 5) && (computerWins < 5) {
+      var playerMove = getPlayerMove ();
+      var computerMove = getComputerMove ();
+      var winner = getWinner (playerMove, computerMove);
+      if (winner === 'player') {
+        console.log('Player wins this round!');
+        playerWins += 1;
+      } else if (winner === 'computer') {
+        console.log('Computer wins this round!');
+        computerWins += 1;
+      } else {
+        console.log('It is a tie!');
+      }
+      console.log('Player chose' + playerMove + 'while Computer chose' + computerMove);
+      console.log('The score is currently' + playerWins + 'to' + computerWins + '/n');
+    }
     return [playerWins, computerWins];
 }
